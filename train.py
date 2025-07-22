ffa214e2f716f3741c40a7b7961008be6ea4d3a8
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2025-07-22 14:51:10 -0400

    use callback in train.py (render every X steps)

--- train.py
+++ train.py
@@ -3,15 +3,23 @@ from stable_baselines3 import PPO
 from callback import RenderCallback
 from racecar_env import RacecarEnv
 
-env = RacecarEnv(get_config())
+env = RacecarEnv(config=get_config(), render_mode="human")
 
 model = PPO(
     policy="MlpPolicy",
     env=env,
     verbose=1,
-    seed=42
-)
+    seed=1)
 
-model.learn(total_timesteps=100000)
+callback = RenderCallback(render_freq=1000)
+try:
+    model.learn(total_timesteps=100000, callback=callback)
+except RuntimeError as e:
+    if str(e) == "Pygame GUI was closed by the user":
+        print("Training stopped")
+    else:
+        raise e
+finally:
+    env.close()
 
 model.save("racecar_ppo_model")
